2102893a247d01c4fd3f6b5e524d37745e1914c6
Abhishek Thapliyal <abhishekthapliyal7579@gmail.com>
2017-06-11 10:47:29 +0530

    #1

--- ECommerceApp/ECommerceApp/ViewController.swift
+++ ECommerceApp/ECommerceApp/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  ECommerceApp
 //
 //  Created by Admin on 10/06/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
